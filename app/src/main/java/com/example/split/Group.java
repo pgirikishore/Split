ab18f5db086933c6591f09d80e695c220198da47
Giri Kishore <girikishore83@gmail.com>
2019-09-05 18:50:34 +0530

    Group layout

---
+++ app/src/main/java/com/example/split/Group.java
@@ -0,0 +1,38 @@
+package com.example.split;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Group extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+        String name= getIntent().getStringExtra("Name");
+
+        mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(name);
+
+
+        
+
+        myViewPager=(ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter =new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout=(TabLayout)findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+}
